1cb82d99004d352f84dc2b4bcc93c0b720594de2
John Axel Eriksson <john@insane.se>
2013-12-24 01:28:49 +0100

    Install java, add comment etc

--- Dockerfile
+++ Dockerfile
@@ -7,4 +7,4 @@ ENV RBENV_MRI 2.0.0-p247
 ENV RBENV_JRUBY jruby-1.7.9
 
 ADD install_ruby.sh /root/install_ruby.sh
-RUN chmod +x /root/install_ruby.sh && /root/install_ruby.sh && rm /root/install_ruby.sh
+RUN chmod +x /root/install_ruby.sh && /root/install_ruby.sh && rm /root/install_ruby.sh # 2013-12-24
--- install_ruby.sh
+++ install_ruby.sh
@@ -1,5 +1,23 @@
 #!/bin/bash
 
+# Fake a fuse install - for java install
+apt-get install --force-yes -y -q adduser libfuse2
+mkdir -p /tmp/fuseinstall
+cd /tmp/fuseinstall ; apt-get download fuse
+cd /tmp/fuseinstall ; dpkg-deb -x fuse_* .
+cd /tmp/fuseinstall ; dpkg-deb -e fuse_*
+cd /tmp/fuseinstall ; rm fuse_*.deb
+cd /tmp/fuseinstall ; echo -en '#!/bin/bash\nexit 0\n' > DEBIAN/postinst
+cd /tmp/fuseinstall ; dpkg-deb -b . /fuse.deb
+cd /tmp/fuseinstall ; dpkg -i /fuse.deb
+
+rm -rf /tmp/fuseinstall
+
+add-apt-repository ppa:webupd8team/java -y
+apt-get update
+echo oracle-java7-installer shared/accepted-oracle-license-v1-1 select true | /usr/bin/debconf-set-selections
+apt-get install --force-yes -y -q oracle-java7-installer
+
 source /etc/profile
 
 upgrade_rbenv
